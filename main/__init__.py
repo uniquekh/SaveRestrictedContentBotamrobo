29357cf2adad6b57c4e65040c3354c94f58498b4
Unique <mujeem000@gmail.com>
2024-06-30 16:54:50 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -16,7 +16,7 @@ API_ID = config("21179966", default=None, cast=int)
 API_HASH = config("d97919fb0a3c725e8bb2a25bbb37d57c", default=None)
 BOT_TOKEN = config("6710743479:AAF1-A8-2tMhzVUP36cYuTA-1q0_vVabmqs", default=None)
 SESSION = config("AQFDLj4ApMgN1F2qrTOYApAsFbYnNBGVVaM2M0tXSGoX-vZ8DExtV7WRxpRo2VdD8IGYV1hn6abC17fX-CY9TgqlfQ1dVKVewD57MBDCJSmj3euOBAwjP7SH0mEGygdlH_WpihZFBp0BMM2VUSTc07IP-0EKR_Nmv0dqMj90mgLSRNJ5MHJV94-XiDWGszYj8fZgdZrczwDuWuhpMnW6PDIXt9T6vbZqgbk85p_cWmYzwf1boL1hnQx-0nqDMU7IOMcp8MG5InRxl1lM0SeVVCU1UMff8ZDkeh0aemYFLHAeQQo9eZ4vDDPesG9CCX7f4k7RrGmafVuXpAmCZ3XIVjRdpmR2_AAAAAG0r_Q_AA", default=None)
-FORCESUB = config("https://t.me/getallmodapk", default=None)
+FORCESUB = config("@getallmodapk", default=None)
 AUTH = config("7326397503", default=None, cast=int)
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
